5d4290e68443d6b078fea5fcc26854e4692de2f5
Kutub Gandhi <kksgandhi@gmail.com>
2019-06-03 16:39:29 -0400

    made scraper helper functions

--- api_scraper.py
+++ api_scraper.py
@@ -1,37 +1,39 @@
 import requests
+import random
 
 ##
 r = requests.get('https://api.sealevelsensors.org/v1.0/Things')
 api_response = r.json()['value']
+api_response
 ##
 
 ##
-for sensor in api_response:
-    print(sensor['name'])
-len(api_response)
+def get_all_sensor_links():
+    r = requests.get('https://api.sealevelsensors.org/v1.0/Things')
+    api_response = r.json()['value']
+    return [x['Datastreams@iot.navigationLink'] for x in api_response]
+
+get_all_sensor_links()
 ##
-print(api_response[1].keys())
+def get_water_obs_links():
+    water_obs_links = []
+    for sensor_link in get_all_sensor_links():
+        obs_type_list = requests.get(sensor_link).json()['value']
+        for obs_type in obs_type_list:
+            if obs_type['name'] == 'Water Level':
+                water_obs_links.append(obs_type['Observations@iot.navigationLink'])
+    return water_obs_links
 
-a_sensor = api_response[7]
-a_sensor
 
-datastream = requests.get(a_sensor['Datastreams@iot.navigationLink']).json()['value'][0]
-datastream
-
-observations = requests.get(datastream['Observations@iot.navigationLink']).json()['value']
-observations
-
-single_obs = observations[1]
-single_obs 
-
-data_in_obs = requests.get(single_obs['Datastream@iot.navigationLink'])
-data_in_obs.json()
-
-multidatastream = requests.get(a_sensor['MultiDatastreams@iot.navigationLink']).json()
-multidatastream
-
-locations = requests.get(a_sensor['Locations@iot.navigationLink']).json()
-locations
-
-historicallocations = requests.get(a_sensor['HistoricalLocations@iot.navigationLink']).json()
-historicallocations
+get_water_obs_links()
+##
+def get_all_obs_for_link(link):
+    observations = requests.get(link).json()
+    value = observations['value']
+    if '@iot.nextLink' in observations:
+        return get_all_obs_for_link(observations['@iot.nextLink']) + value
+    else:
+        return value
+##
+water_obs = random.choice(get_water_obs_links())
+len(get_all_obs_for_link(water_obs))
