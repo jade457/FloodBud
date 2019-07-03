6cd774efb31d26076a70a86f8d665797e757c08b
Kutub Gandhi <kksgandhi@gmail.com>
2019-07-03 10:51:52 -0400

    api_scraper is now very functional programy

--- notebooks/api_scraper.py
+++ notebooks/api_scraper.py
@@ -2,31 +2,28 @@ import requests as req
 import dateutil.parser as date_parser
 from pprint import pprint as print
 
-base_url      = 'https://api.sealevelsensors.org/v1.0/Things'
+base_url_sls  = 'https://api.sealevelsensors.org/v1.0/Things'
 base_url_noaa = 'https://tidesandcurrents.noaa.gov/api/datagetter'
 
-def get_all_sensor_links():
+def get_sensor_datastreams():
     """
     Creates a list of all datastream links.
     Helper function for get_water_obs_links
     Exported in case anyone wants to use it
     """
-    api_response = req.get(base_url).json()
-    return_list = []
-    for sensor in api_response["value"]:
+    api_response = req.get(base_url_sls).json()
+    def create_sensor_obj(sensor):
         location = (req.get(sensor["Locations@iot.navigationLink"])).json()["value"][0]
-
         coordinates = location["location"]["coordinates"]
-        return_list.append({
+        return {
           "name":   sensor["name"],
           "desc":   sensor["description"],
           "link":   sensor["Datastreams@iot.navigationLink"],
           "elev":   sensor["properties"].get("elevationNAVD88"),
-          "coords": coordinates
-          })
-    return return_list
+          "coords": coordinates}
+    return list(map(create_sensor_obj, api_response["value"]))
 
-def get_water_obs_links():
+def get_sensors_with_water():
   """
   Returns a list of links that
   lead to water sensor observations.
@@ -34,10 +31,25 @@ def get_water_obs_links():
   """
   water_obs_links = []
   # get all the datastream links
-  all_sensor_links = get_all_sensor_links()
+  all_sensor_links = get_sensor_datastreams()
   """
   these links contain all types of observations
   we need to filter only water level observations
+  """
+  def get_water_link_from_sensor(sensor):
+      obs_type_list = req.get(sensor["link"]).json()["value"]
+      only_water_list = list(filter(lambda obs_type: obs_type["name"] == "Water Level", obs_type_list))
+      if len(only_water_list) == 0:
+          return
+      only_water = only_water_list[0]
+      return {
+          "name":   sensor["name"],
+          "desc":   sensor["desc"],
+          "elev":   sensor["elev"],
+          "coords": sensor["coords"],
+          "link":   only_water['Observations@iot.navigationLink']}
+  return list(filter(None, map(get_water_link_from_sensor, all_sensor_links)))
+
   """
   for sensor in all_sensor_links:
       obs_type_list = req.get(sensor["link"]).json()
@@ -50,6 +62,7 @@ def get_water_obs_links():
                   "coords": sensor["coords"],
                   "link":   obs_type['Observations@iot.navigationLink']})
   return water_obs_links
+  """
 
 def get_obs_for_link(link, start_date = None, end_date = None):
     """
@@ -59,9 +72,11 @@ def get_obs_for_link(link, start_date = None, end_date = None):
     start_date and end_date are optional
     end date is even more optional than start date
     """
-    filters = {
-        "$select":       "resultTime,result",
-        "$resultFormat": "dataArray"}
+    is_iot_next_link = "?" in link
+    params = {}
+    if not is_iot_next_link:
+        params["$select"]       =  "resultTime,result"
+        params["$resultFormat"] =  "dataArray"
     """
     the next few lines look ugly, but that's just
     string formatting for the urls in the GET request
@@ -73,12 +88,11 @@ def get_obs_for_link(link, start_date = None, end_date = None):
     end_date   = date_parser.parse(end_date).isoformat()   + "Z" if end_date   else None
 
     if start_date and end_date:
-        filters["$filter"] = "resultTime ge " + start_date + " and resultTime le " + end_date
+        params["$filter"] = "resultTime ge " + start_date + " and resultTime le " + end_date
     elif start_date:
-        filters["$filter"] = "resultTime ge " + start_date
+        params["$filter"] = "resultTime ge " + start_date
 
-    response = (req.get(link).json() if "?" in link
-                else req.get(link, params = filters).json())
+    response = req.get(link, params = params).json()
     if len(response["value"]) == 0:
         return []
     unparsed_observations = response["value"][0]["dataArray"]
@@ -88,8 +102,8 @@ def get_obs_for_link(link, start_date = None, end_date = None):
     so we need to get the rest. Luckily it also returns
     @iot.nextLink which is a link to the next 100
     we use that link to recursively get all the observations we need
-    We don't need to deal with filters because the @iot.nextLink
-    includes all the filters
+    We don't need to deal with params because the @iot.nextLink
+    includes all the params
     """
     if "@iot.nextLink" in response:
         all_observations = get_obs_for_link(response['@iot.nextLink']) + observations
@@ -98,19 +112,12 @@ def get_obs_for_link(link, start_date = None, end_date = None):
         might be inefficient because of sorting at each recursion,
         but who cares
         """
-        if not "?" in link:
+        if not is_iot_next_link:
             return sorted(all_observations, key=lambda x: x[1])
         return all_observations
     else:
         # No iot next link? Must be the end, return
         return observations
-
-def get_obs_for_links(links, start_date = None, end_date = None):
-    """
-    simple wrapper function that takes in a list of links
-    """
-    promises_array = map(lambda link: get_obs_for_link(link, start_date, end_date), links)
-    return promises_array
 
 def get_ft_pulaski(start_date, end_date):
     params = {
@@ -122,14 +129,16 @@ def get_ft_pulaski(start_date, end_date):
         "units":       "metric",
         "format":      "json"
     }
-    params["begin_date"] = date_parser.parse(start_date).strftime("%Y%m%d %H:%M")
-    params["end_date"]   = date_parser.parse(end_date).strftime("%Y%m%d %H:%M")
+    format_time = lambda date: (date_parser
+                                    .parse(date)
+                                    .strftime("%Y%m%d %H:%M"))
+
+    params["begin_date"] = format_time(start_date)
+    params["end_date"]   = format_time(end_date)
 
     return req.get(base_url_noaa, params=params).json()["predictions"]
 
-
 if __name__ == "__main__":
-    lonks = get_water_obs_links()
-    
-    print(get_obs_for_link(lonks[3]["link"], "April 3 2019", "April 5 2019"))
-    print(get_ft_pulaski("April 3 2019", "April 5 2019"))
+    waaata = get_sensors_with_water()
+    # aa = get_obs_for_link(waaata[6]["link"], "April 1 2019", "April 3 2019")
+    print(get_ft_pulaski("April 1 2019", "April 3 2019"))
